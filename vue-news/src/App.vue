32e8337bf781fadd3f4d9dc58e960e4db2d5b5b8
sonseong10 <sonseong10@gmail.com>
2021-05-02 20:51:51 +0900

    🎉 Create project vue-news

---
+++ vue-news/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    hello!!!!
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  methods: {
+    fetchData() {
+      console.log("hi")
+    }
+  }
+}
+</script>
+
+<style></style>
