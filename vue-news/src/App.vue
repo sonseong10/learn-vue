f8baab15b41e3fdf0fa6c58aa2db08c669ec26dd
sonseong10 <sonseong10@gmail.com>
2021-05-03 14:39:21 +0900

    🚧 Implement global header and reset base style

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -1,12 +1,53 @@
 <template>
   <div id="app">
+    <gnb-header></gnb-header>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-/* eslint-disable */
-export default {}
+import GnbHeader from "./components/GnbHeader.vue"
+
+export default {
+  components: {
+    GnbHeader
+  }
+}
 </script>
 
-<style></style>
+<style>
+* {
+  box-sizing: border-box;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+body {
+  padding: 0;
+  margin: 0;
+}
+
+ul,
+ol {
+  margin: 0;
+  padding-left: 0;
+  list-style: none;
+}
+
+a {
+  text-decoration: none;
+  color: #000;
+}
+
+.visually-hidden {
+  position: absolute !important;
+  width: 1px !important;
+  height: 1px !important;
+  padding: 0 !important;
+  margin: -1px !important;
+  overflow: hidden !important;
+  clip: rect(0, 0, 0, 0) !important;
+  white-space: nowrap !important;
+  border: 0 !important;
+}
+</style>
---
+++ vue-news/src/components/GnbHeader.vue
@@ -0,0 +1,61 @@
+<template>
+  <header class="global-header">
+    <nav class="gnb">
+      <h2 class="visually-hidden">GNB</h2>
+      <ul class="gnb-list">
+        <li class="gnb-item">
+          <router-link class="gnb-item-link" to="/news">News</router-link>
+        </li>
+        <li class="gnb-item">
+          <router-link class="gnb-item-link" to="/ask">Ask</router-link>
+        </li>
+        <li class="gnb-item">
+          <router-link class="gnb-item-link" to="/jobs">Jobs</router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.global-header {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  height: 50px;
+  padding: 0 8px;
+  background-color: #42b883;
+}
+
+.gnb-list {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+
+.gnb-item:not(:last-child)::after {
+  margin: 0;
+  color: #fff;
+  content: "|";
+}
+
+.gnb-item-link {
+  padding: 8px;
+  font-weight: 700;
+  color: #fff;
+  transition: color 300ms ease-in-out;
+}
+
+.gnb-item-link:hover {
+  color: #354953;
+}
+
+.router-link-active {
+  color: #354953;
+  text-decoration: underline;
+}
+</style>
