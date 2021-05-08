dedd89c6c8b88586a504584f5ef494a93fc704de
sonseong10 <sonseong10@gmail.com>
2021-05-09 01:22:22 +0900

    ♻️ Update api async/await connections

--- vue-news/src/service/api/news-api.js
+++ vue-news/src/service/api/news-api.js
@@ -4,16 +4,31 @@ const config = {
   baseUrl: "https://api.hnpwa.com/v0/"
 }
 
-function fetchList(pageName) {
-  return axios.get(`${config.baseUrl}${pageName}/1.json`)
+async function fetchList(pageName) {
+  try {
+    const response = await axios.get(`${config.baseUrl}${pageName}/1.json`)
+    return response
+  } catch (error) {
+    console.log(error)
+  }
 }
 
-function fetchUser(userId) {
-  return axios.get(`${config.baseUrl}user/${userId}.json`)
+async function fetchUser(userId) {
+  try {
+    const response = await axios.get(`${config.baseUrl}user/${userId}.json`)
+    return response
+  } catch (error) {
+    console.log(error)
+  }
 }
 
-function fetchItem(itemId) {
-  return axios.get(`${config.baseUrl}item/${itemId}.json`)
+async function fetchItem(itemId) {
+  try {
+    const response = await axios.get(`${config.baseUrl}item/${itemId}.json`)
+    return response
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 export { fetchList, fetchUser, fetchItem }
--- vue-news/src/store/modules/actions.js
+++ vue-news/src/store/modules/actions.js
@@ -1,21 +1,30 @@
 import { fetchList, fetchUser, fetchItem } from "../../service/api/news-api"
 
 export default {
-  FETCH_LIST({ commit }, pageName) {
-    return fetchList(pageName)
-      .then(({ data }) => commit("SET_LIST", data))
-      .catch(error => console.log(error.response))
+  async FETCH_LIST({ commit }, pageName) {
+    try {
+      const { data } = await fetchList(pageName)
+      return commit("SET_LIST", data)
+    } catch (error) {
+      return console.log(error.response)
+    }
   },
 
-  FETCH_USER({ commit }, uaserid) {
-    return fetchUser(uaserid)
-      .then(({ data }) => commit("SET_USER", data))
-      .catch(error => console.log(error.response))
+  async FETCH_USER({ commit }, uaserid) {
+    try {
+      const { data } = await fetchUser(uaserid)
+      return commit("SET_USER", data)
+    } catch (error) {
+      return console.log(error.response)
+    }
   },
 
-  FETCH_ITEM({ commit }, itemid) {
-    return fetchItem(itemid)
-      .then(({ data }) => commit("SET_ITEM", data))
-      .catch(error => console.log(error.response))
+  async FETCH_ITEM({ commit }, itemid) {
+    try {
+      const { data } = await fetchItem(itemid)
+      return commit("SET_ITEM", data)
+    } catch (error) {
+      return console.log(error.response)
+    }
   }
 }
