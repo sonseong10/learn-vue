5e947aff4cf90713c080e058f28ea742e3cdc0ca
sonseong10 <sonseong10@gmail.com>
2021-05-12 13:25:48 +0900

    🚧 Implement vue ruter and page views

--- vue-til/src/App.vue
+++ vue-til/src/App.vue
@@ -1,5 +1,11 @@
 <template>
-	<div>hello</div>
+	<div>
+		<header>
+			<router-link to="/login">Login</router-link>
+			<router-link to="/signup">Signup</router-link>
+		</header>
+		<router-view></router-view>
+	</div>
 </template>
 
 <script>
