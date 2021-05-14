205f96ed417c0e071ae2b1f3d88ac27f71a7a461
sonseong10 <sonseong10@gmail.com>
2021-05-14 20:06:11 +0900

    🚧 Apply component SignupForm.vue

--- vue-til/src/views/SignupPage.vue
+++ vue-til/src/views/SignupPage.vue
@@ -1,9 +1,15 @@
 <template>
-	<div>signup</div>
+	<SignupForm></SignupForm>
 </template>
 
 <script>
-export default {};
+import SignupForm from '@/components/SignupForm.vue';
+export default {
+	components: {
+		SignupForm,
+	},
+};
 </script>
 
 <style></style>
+SingupForm
