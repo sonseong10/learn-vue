8727471bea6f1473ad7a3a764fe2f502bafb4da8
sonseong10 <sonseong10@gmail.com>
2021-06-11 15:35:01 +0900

    💄 Implement Icon button for router link

--- vue-til/src/views/MainPage.vue
+++ vue-til/src/views/MainPage.vue
@@ -1,15 +1,61 @@
 <template>
 	<div class="container">
 		<h1 class="form-title">Today I Learned</h1>
+		<router-link class="icon-button" to="/add">
+			<PlusIcon></PlusIcon>
+		</router-link>
 		<PostList></PostList>
 	</div>
 </template>
 
 <script>
 import PostList from '@/components/posts/PostList.vue';
+import { PlusIcon } from 'vue-feather-icons';
+
 export default {
-	components: { PostList },
+	components: { PostList, PlusIcon },
 };
 </script>
 
-<style></style>
+<style scoped>
+.icon-button {
+	position: absolute;
+	right: 30px;
+	bottom: 40px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 40px;
+	height: 40px;
+	background-color: #40b381;
+	font-size: 15px;
+	color: #fff;
+	border: 3px solid #fff;
+	border-radius: 50%;
+	box-shadow: 4px 4px 6px rgb(0 0 0 / 6%);
+	z-index: 100;
+	transition: background-color 300ms ease-in-out;
+}
+
+.icon-button:active {
+	background-color: #008756;
+}
+
+@media screen and (min-width: 768px) {
+	.icon-button {
+		font-size: 18px;
+		width: 50px;
+		height: 50px;
+	}
+
+	.icon-button:hover {
+		background-color: #008756;
+	}
+}
+
+@media screen and (min-width: 1200px) {
+	.icon-button {
+		right: 18%;
+	}
+}
+</style>
