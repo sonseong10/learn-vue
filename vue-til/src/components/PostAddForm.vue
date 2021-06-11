bdabfa2f3863fb2170943694cdff3bdb3cd48c32
sonseong10 <sonseong10@gmail.com>
2021-06-11 21:54:38 +0900

    🚧 Implement post add page components

---
+++ vue-til/src/components/PostAddForm.vue
@@ -0,0 +1,90 @@
+<template>
+	<form @submit.prevent="submitForm">
+		<div class="input-box">
+			<label for="title">Title</label>
+			<input id="title" type="text" v-model="title" />
+		</div>
+		<div class="input-box">
+			<label for="contents">Contents</label>
+			<textarea
+				id="contents"
+				type="text"
+				rows="5"
+				v-model="contents"
+				placeholder="200자 이내"
+			/>
+		</div>
+
+		<button class="form-button" type="submit" :disabled="!contentsValid()">
+			등록
+		</button>
+		<strong class="validation-text" v-if="!contentsValid()">
+			<span class="warning"> 200자 이상 입력 하셨습니다. </span>
+		</strong>
+		<strong class="validation-text">
+			<span class="warning">
+				{{ errorMsg }}
+			</span>
+		</strong>
+	</form>
+</template>
+
+<script>
+import { createPost } from '@/api/auth';
+
+export default {
+	data() {
+		return {
+			title: '',
+			contents: '',
+			errorMsg: '',
+		};
+	},
+	methods: {
+		async submitForm() {
+			try {
+				const postData = {
+					title: this.title,
+					contents: this.contents,
+				};
+				const response = await createPost(postData);
+				console.log(response);
+			} catch (error) {
+				const status = error.response.status;
+				if (status === 400) {
+					this.errorMsg = '동일한 제목의 게시물이 이미 있습니다.';
+				}
+				console.log(status);
+			} finally {
+				this.initForm();
+			}
+		},
+
+		initForm() {
+			this.title = '';
+			this.contents = '';
+		},
+
+		contentsValid() {
+			return this.contents.length <= 200;
+		},
+	},
+};
+</script>
+
+<style scoped>
+.input-box {
+	padding: 10px;
+	height: 100%;
+}
+
+.form-button {
+	position: absolute;
+	right: 10px;
+}
+
+.validation-text {
+	display: block;
+	text-align: center;
+}
+</style>
---
+++ vue-til/src/views/PostAddPage.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="container">
+		<h1 class="page-title">Add Page</h1>
+		<PostAddForm></PostAddForm>
+	</div>
+</template>
+
+<script>
+import PostAddForm from '@/components/PostAddForm.vue';
+export default {
+	components: { PostAddForm },
+};
+</script>
+
+<style></style>
