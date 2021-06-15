e73cebbb7d4e8c0db5f4707fc58edb87d3e98427
sonseong10 <sonseong10@gmail.com>
2021-06-15 16:58:37 +0900

    ✅ Update testing in LoginForm component

--- vue-til/src/components/LoginForm.spec.js
+++ vue-til/src/components/LoginForm.spec.js
@@ -1,10 +1,29 @@
-import Vue from 'vue';
+import { shallowMount } from '@vue/test-utils';
 import LoginForm from './LoginForm.vue';
 
 describe('LoginForm.vue', () => {
-	test('component is mounted, username must exist and be set to its initial value.', () => {
-		const instance = new Vue(LoginForm).$mount();
-		console.log(instance.username);
-		expect(instance.username).toBe('');
+	test('ID must be in email format.', () => {
+		const wrapper = shallowMount(LoginForm, {
+			data() {
+				return {
+					username: 'test@abc.com',
+				};
+			},
+		});
+		const warningText = wrapper.find('.warning');
+		expect(warningText.exists()).toBeTruthy();
+	});
+
+	test('id pw value nall login btn disabled', () => {
+		const wrapper = shallowMount(LoginForm, {
+			data() {
+				return {
+					username: '',
+					password: '',
+				};
+			},
+		});
+		const button = wrapper.find('button');
+		expect(button.element.disabled).toBeTruthy();
 	});
 });
