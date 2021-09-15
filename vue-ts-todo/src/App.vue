5632352895cfbf6aaf0cd960af7e367d1887f72f
sonseong10 <sonseong10@gmail.com>
2021-09-15 20:42:00 +0900

    🚧 Implement a function to todo items

--- vue-ts-todo/src/App.vue
+++ vue-ts-todo/src/App.vue
@@ -23,10 +23,24 @@ import Vue from 'vue';
 import TodoInput from './components/TodoInput.vue';
 import TodoItem from './components/TodoItem.vue';
 
+const STOREGE_KEY = 'uid-1';
+const storege = {
+  save(value: any[]) {
+    const parsed = JSON.stringify(value);
+    localStorage.setItem(STOREGE_KEY, parsed);
+  },
+  fetch() {
+    const todoItems = localStorage.getItem(STOREGE_KEY) || '[]';
+    const result = JSON.parse(todoItems);
+    return result;
+  },
+};
+
 export default Vue.extend({
   data() {
     return {
       todoText: '',
+      todoItems: [] as any,
     };
   },
   components: { TodoInput, TodoItem },
@@ -36,13 +50,19 @@ export default Vue.extend({
     },
     addTodoItem() {
       const value = this.todoText;
-
-      localStorage.setItem(value, value);
+      this.todoItems.push(value);
+      storege.save(this.todoItems);
       this.resetTodoValue();
     },
     resetTodoValue() {
       this.todoText = '';
     },
+    fetchStorege() {
+      this.todoItems = storege.fetch();
+    },
+  },
+  created() {
+    this.fetchStorege();
   },
 });
 </script>
