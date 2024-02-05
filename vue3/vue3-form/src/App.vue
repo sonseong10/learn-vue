157805f9e2fcb59c3f74e351432fad020724d788
sonseongyeol <syson@fastviewkorea.com>
2024-02-05 15:03:24 +0900

    프로젝트 기초 설정

---
+++ vue3/vue3-form/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <form action="">
+    <div>
+      <label for="userId">ID</label>
+      <input type="text" id="userId" />
+    </div>
+    <div>
+      <label for="userPassword">PW</label>
+      <input type="text" id="userPassword" />
+    </div>
+    <button type="submit">로그인</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
