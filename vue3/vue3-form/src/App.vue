fa3a814c0f0ba88ee730e7166ee4607cb9e78a76
sonseongyeol <syson@fastviewkorea.com>
2024-02-15 13:27:35 +0900

    Vue3 composition API 학습

--- vue3/vue3-form/src/App.vue
+++ vue3/vue3-form/src/App.vue
@@ -1,20 +1,58 @@
 <template>
-  <form action="">
+  <form action="" @submit.prevent="submitForm">
     <div>
       <label for="userId">ID</label>
-      <input type="text" id="userId" />
+      <input type="text" id="userId" v-model="userName" />
     </div>
     <div>
       <label for="userPassword">PW</label>
-      <input type="text" id="userPassword" />
+      <input type="text" id="userPassword" v-model="userPassword" />
     </div>
     <button type="submit">로그인</button>
   </form>
 </template>
 
 <script>
+import axios from "axios";
+import { ref } from "vue";
 export default {
-  name: "App",
+  setup() {
+    const userName = ref("");
+    const userPassword = ref("");
+
+    const submitForm = () => {
+      axios
+        .post("https://jsonplaceholder.typicode.com/users/", {
+          userName: userName.value,
+          password: userPassword.value,
+        })
+        .then((res) => {
+          console.log(res);
+        });
+    };
+
+    return { userName, userPassword, submitForm };
+  },
+
+  // data() {
+  //   return {
+  //     userName: "",
+  //     userPassword: "",
+  //   };
+  // },
+
+  // methods: {
+  //   submitForm() {
+  //     axios
+  //       .post("https://jsonplaceholder.typicode.com/users/", {
+  //         userName: this.userName,
+  //         password: this.userPassword,
+  //       })
+  //       .then((res) => {
+  //         console.log(res);
+  //       });
+  //   },
+  // },
 };
 </script>
 
